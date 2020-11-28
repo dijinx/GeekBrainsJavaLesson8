5cecb833a9829950f14140ebdfc57977b823a3a3
Dijinx <dimajinx@gmail.com>
2020-11-28 14:36:56 +0700

    Initial commit

---
+++ src/test/java/Desktop.java
@@ -0,0 +1,87 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//Довольно удручающе получилось ))
+
+public class Desktop extends JFrame {
+	public Desktop() {
+		setTitle("Test Window");
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setBounds(300, 300, 800, 400);
+		setLayout(new FlowLayout());
+
+		final JTextArea textArea = new JTextArea();
+
+
+		JButton button = new JButton("Начните с этой");
+		add(button, BorderLayout.PAGE_START);
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String message = "Спасибо за ваши лекции!";
+				textArea.setText(message);
+			}
+		});
+
+		final JButton button1 = new JButton("Вторая");
+		add(button1, BorderLayout.PAGE_START);
+		button1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String message1 = "Мне довольно трудно угнаться за программой обучения.";
+				textArea.setText(message1);
+			}
+		});
+
+		final JButton button2 = new JButton("Третья");
+		add(button2, BorderLayout.PAGE_START);
+		button2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String message2 = "Чуть больше времени мне понадобится для освоения джавы.";
+				textArea.setText(message2);
+			}
+		});
+
+		final JButton button3 = new JButton("Четвертая");
+		add(button3, BorderLayout.PAGE_START);
+		button3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String message3 = "Но, я понимаю что я способен всё это выучить.";
+				textArea.setText(message3);
+			}
+		});
+
+		final JButton button4 = new JButton("Пятая");
+		add(button4, BorderLayout.PAGE_START);
+		button4.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String message4 = "Всё у меня получится.";
+				textArea.setText(message4);
+			}
+		});
+
+		final JButton button5 = new JButton("Заключительная");
+		add(button5, BorderLayout.PAGE_START);
+		button5.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String message5 = "Тут вдохновляющая музыка и титры. Конец.";
+				textArea.setText(message5);
+			}
+		});
+
+		add(textArea);
+		setVisible(true);
+	}
+}
+
+class MainClass {
+	public static void main(String[] args) {
+		Desktop Decktop = new Desktop();
+	}
+}
